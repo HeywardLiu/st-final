cec3fa0a705c9b191136fb9be8fefda80838eea6
Laxiflora <liu2000113@gmail.com>
2023-06-06 19:14:09 +0800

    finish all retrieve method unit test

--- _test_MongoDB.py
+++ _test_MongoDB.py
@@ -33,6 +33,70 @@ class TestMongoDB(unittest.TestCase):
                     result=[x for x in self.db.retrieve_earthquake_data_by_factory(factory=case['factory'], quantity=case['quantity'])]
                     self.assertEqual(result, case['outcome'])
 
+    def test_retrieve_earthquake_data(self):
+        self.setup()
+        self.testcases=self.testcases["test_retrieve_earthquake_data"]
+        self.db.insert_earthquake_data=MagicMock(side_effect=self.mock_insert_earthquake)
+        for case in self.testcases:
+            with self.subTest(case['case']):
+                self.mock_reset()
+                if(case.get("mock_insert_data") != None):
+                    self.db.insert_earthquake_data(case["mock_insert_data"])
+                if("Exception" in case["outcome"]):
+                    e = case["outcome"].split(":")[1]
+                    with self.assertRaises(eval(e)):
+                        self.db.retrieve_earthquake_data(quantity=case['quantity'])
+                else:
+                    result=[x for x in self.db.retrieve_earthquake_data(quantity=case['quantity'])]
+                    print(result)
+                    self.assertEqual(result, case['outcome'])
+
+
+    def test_retrieve_reservoir_data_by_name(self):
+        self.setup()
+        self.testcases=self.testcases["test_retrieve_reservoir_data_by_name"]
+        self.db.insert_reservoir_data=MagicMock(side_effect=self.mock_insert_reservoir)
+        for case in self.testcases:
+            with self.subTest(case['case']):
+                self.mock_reset()
+                if(case.get("mock_insert_data") != None):
+                    self.db.insert_reservoir_data(case["mock_insert_data"])
+                if("Exception" in case["outcome"]):
+                    e = case["outcome"].split(":")[1]
+                    with self.assertRaises(eval(e)):
+                        self.db.retrieve_reservoir_data_by_name(name=case['name'], quantity=case['quantity'])
+                else:
+                    result=[x for x in self.db.retrieve_reservoir_data_by_name(name=case['name'], quantity=case['quantity'])]
+                    print(result)
+                    self.assertEqual(result, case['outcome'])
+
+    #TODO
+    def test_retrieve_electricity_data_by_region(self):
+        self.setup()
+        self.testcases=self.testcases["retrieve_electricity_data_by_region"]
+        self.db.insert_electricity_data=MagicMock(side_effect=self.mock_insert_electricity)
+        for case in self.testcases:
+            with self.subTest(case['case']):
+                self.mock_reset()
+                if(case.get("mock_insert_data") != None):
+                    self.db.insert_electricity_data(case["mock_insert_data"])
+                if("Exception" in case["outcome"]):
+                    e = case["outcome"].split(":")[1]
+                    with self.assertRaises(eval(e)):
+                        self.db.retrieve_electricity_data_by_region(region=case['region'], quantity=case['quantity'])
+                else:
+                    result=[x for x in self.db.retrieve_electricity_data_by_region(region=case['region'], quantity=case['quantity'])]
+                    print(result)
+                    self.assertEqual(result, case['outcome'])
+
+
+    def mock_insert_electricity(self, data):
+        self.db.insert_data(collection_name="electricity", data=data)
+
+
+    def mock_insert_reservoir(self, data):
+        self.db.insert_data(collection_name="reservoir", data=data)
+
     def mock_insert_earthquake(self, data):
         # Insert data into "factory" collection
         insert_list = []
